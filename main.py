355e90b654d27502bf1f7eb25d0434c5fe97d5da
berrabe@manjaro <rizalical250@gmail.com>
2020-12-02 13:12:01 +0700

    UPDATE DB FITURE

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from src import *
+"""
+main module
+"""
+
+import time
 import logging
+from src import mod_bi
+
+logger = logging.getLogger(__name__)
+
+
 
 if __name__ == '__main__':
-	logging.basicConfig(level = logging.INFO, 
+	logging.basicConfig(level = logging.INFO,
 						# filename='bi.log', filemode='w',
-						format = '[ %(levelname)s ] [ %(name)s ] [ %(asctime)s ] => %(message)s', 
+						format = '[ %(levelname)s ] [ %(name)-10s ] [ %(asctime)s ] => %(message)s',
 						datefmt='%d-%b-%y %H:%M:%S')
-	
+
+	start_time = time.time()
+	logger.info("================== Logger Start ==================")
+
 	obj = mod_bi.bi('https://www.bi.go.id/id/moneter/informasi-kurs/transaksi-bi/Default.aspx')
-	# obj.table()
-	obj.send_db('localhost','root','berrabe','bi')
+	obj.send_db('localhost','user_bi','pass_bi','db_bi')
+	obj.table()
+
+	logger.info("================== Done @ %.2f Second ==================\n\n",
+		time.time() - start_time)
--- src/mod_bi.py
+++ src/mod_bi.py
@@ -1,43 +1,73 @@
+"""
+Modul Where The Magic Happen
+- berrabe
+"""
+import datetime
+import logging
+import sys
 from bs4 import BeautifulSoup
 import mysql.connector
 import requests
-import time
 import tabulate
-import logging
-import sys
+
 
 logger = logging.getLogger(__name__)
 
-class bi():
-	"""docstring for bi"""
+
+
+
+class BI():
+	"""docstring for class bi"""
 
 	def __init__(self, url):
+		"""
+		Of Course, This is initialization method
+		"""
+
 		try:
-			logger.info(f"BI MODULE - { url }")
 			self.list_uang = [[]]
-			self.__URL__ = url
+			self.raw_data = None
+			self.url_ = url
+
+			self.__req()
 			self.__get_kurs()
 			self.__get_abr()
-		except Exception as e:
-			logger.exception(f'INIT ERROR { e }')
 
-
-	def __req(self):
-		try:
-			logger.info("Get Data From BI")
-			page = requests.get(self.__URL__, timeout=5)
-			if page.status_code == 200:
-				logger.info(f"Get Data From BI SUCCESS - { page.status_code }")
-				return BeautifulSoup(page.content, 'lxml')
-		except Exception as e:
-			logger.exception(f'GET DATA ERROR { e }')
+		except Exception:
+			logger.exception('INIT ERROR')
 			sys.exit(2)
 
 
-	def __get_kurs(self):
+
+
+	def __req(self):
+		"""
+		method for make conn for getting / sraping
+		info from bi website
+		"""
+
 		try:
-			logger.info("Getting Kurs")
-			soup = self.__req()
+			page = requests.get(self.url_, timeout=5)
+
+			if page.status_code == 200:
+				logger.info("Get Data From BI SUCCESS - %s", page.status_code)
+				self.raw_data = BeautifulSoup(page.content, 'lxml')
+
+		except Exception:
+			logger.exception('GET DATA ERROR')
+			sys.exit(2)
+
+
+
+
+	def __get_kurs(self):
+		"""
+		Method For Parsing Kurs From BI Website
+		"""
+
+		try:
+			logger.info("Getting Current Kurs")
+			soup = self.raw_data
 			table = soup.find("table", attrs={"class": "table1"})
 			data = table.find_all("td")
 
@@ -46,19 +76,27 @@ class bi():
 				if i % 5 == 0 and i != 0:
 					self.list_uang.append([])
 					index += 1
-				
+
 				if data[i].text == "\n\n":
 					continue
 
 				self.list_uang[index].append(data[i].text)
-		except Exception as e:
-			logger.exception(f'GET KURS ERROR { e }')
+
+		except Exception:
+			logger.exception('GET KURS ERROR')
+			sys.exit(2)
+
+
 
 
 	def __get_abr(self):
+		"""
+		Method For Getting Abbreviation
+		"""
+
 		try:
 			logger.info("Getting List Available Currency")
-			soup = self.__req()
+			soup = self.raw_data
 			table = soup.find("div", attrs={"id": "KodeSingkatan"})
 			name = table.find_all("td")
 
@@ -68,40 +106,71 @@ class bi():
 					splitting = name[i].text.split()
 					self.list_uang[index].insert(0, ' '.join(splitting))
 					index += 1
-		except Exception as e:
-			logger.exception(f'GET LIST AVAILABLE CURRENCY ERROR { e }')
+
+		except Exception:
+			logger.exception('GET LIST AVAILABLE CURRENCY ERROR')
+			sys.exit(2)
+
+
+
 
 	def table(self):
+		"""
+		Method For show the final data to terminal with table
+		for ease of reading
+		"""
+
 		try:
 			logger.info("Show Currency Table")
 			headers = ["Mata Uang", "ABR", "Nilai", "Kurs Jual", "Kurs Beli"]
+
 			print(tabulate.tabulate(self.list_uang, headers, tablefmt="pretty"))
-		except Exception as e:
-			logger.exception(f'SHOW TABLE Error { e }')
 
-	def send_db(self, host='localhost', user='root', password='root', database='bi'):
+		except Exception:
+			logger.exception('SHOW TABLE Error')
+			sys.exit(2)
+
+
+
+
+	def send_db(self, host='localhost', user='user_bi', password='pass_bi', database='db_bi'):
+		"""
+		Method For Handling All DB Stuff
+		"""
+
 		try:
-			logger.info("Send DB of Kurs Lists")
+			logger.info("Send Current Kurs To DB")
+			date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-			db = mysql.connector.connect(
+			db_ = mysql.connector.connect(
 				host=host,
 				user=user,
 				password=password,
-				database=database
-			)
+				database=database)
+			cur = db_.cursor()
 
-			cur = db.cursor()
+			cur.execute("""CREATE TABLE IF NOT EXISTS `KURS` (
+			`No` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`Waktu` datetime NOT NULL,
+			`Mata_Uang` varchar(20) NOT NULL,
+			`ABR` varchar(20) NOT NULL,
+			`Nilai` decimal(20) NOT NULL,
+			`Kurs_Jual` varchar(20) NOT NULL,
+			`Kurs_Beli` varchar(20) NOT NULL)
+			""")
 
-			for i in self.list_uang: 
 
-				sql = f"""INSERT INTO `terbaru` 
-				(`No`, `Mata Uang`, `ABR`, `Nilai`, `Kurs Jual`, `Kurs Beli`) 
+			for i in self.list_uang:
+
+				sql = f"""INSERT INTO `KURS`
+				(`No`, `Waktu`, `Mata_Uang`, `ABR`, `Nilai`, `Kurs_Jual`, `Kurs_Beli`) 
 				VALUES 
-				(NULL, '{i[0]}', '{i[1]}', '{i[2]}', '{i[3]}', '{i[4]}'); """
+				(NULL, '{date}', '{i[0]}', '{i[1]}', '{i[2]}', '{i[3]}', '{i[4]}'); """
 
 				cur.execute(sql)
 
-			db.commit()
+			db_.commit()
 
-		except Exception as e:
-			logger.exception(f'SEND DB ERROR { e }')
+		except Exception:
+			logger.exception('SEND DB ERROR')
+			sys.exit(2)
